5348111f0d1d84a6042826f4d59aacc363ee9915
Krishiv111 <krishivmahendru2007@gmail.com>
2024-01-29 19:34:04 -0800

    pushing code that was broken

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 @RequestMapping("/api/person")
 public class PersonApiController {
     //     @Autowired
